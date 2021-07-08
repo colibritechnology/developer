40fd7581cf9d4dad6dfdbf6665325c17a53835c8
colibri <adm@colibri.pe>
2021-07-07 21:47:40 -0500

    initial commit

---
+++ articulos/views.py
@@ -0,0 +1,40 @@
+# articles/views.py
+from django.contrib.auth.mixins import (LoginRequiredMixin,UserPassesTestMixin )
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import UpdateView, DeleteView,CreateView
+from django.urls import reverse_lazy
+from .models import Article
+
+class ArticleListView(LoginRequiredMixin,ListView):
+    model = Article
+    template_name = 'article_list.html'
+
+class ArticleDetailView(LoginRequiredMixin,DetailView): # new
+    model = Article
+    template_name = 'article_detail.html'
+
+class ArticleUpdateView(LoginRequiredMixin,UserPassesTestMixin,UpdateView): # new
+    model = Article
+    fields = ('title', 'body',)
+    template_name = 'article_edit.html'
+    def test_func(self):
+        obj = self.get_object()
+        return obj.author == self.request.user#el owner del objeto == al usuario logeado
+
+class ArticleDeleteView(LoginRequiredMixin,UserPassesTestMixin,DeleteView): # new
+    model = Article
+    template_name = 'article_delete.html'
+    success_url = reverse_lazy('article_list')
+
+    def test_func(self):
+        obj = self.get_object()
+        return obj.author == self.request.user#el owner del objeto == al usuario logeado
+
+class ArticleCreateView(LoginRequiredMixin,CreateView): # new
+    model = Article
+    template_name = 'article_new.html'
+    fields = ('title', 'body',)    
+
+    def form_valid(self, form): # new
+        form.instance.author = self.request.user
+        return super().form_valid(form)

f6714a952aafad76e4c936d2de7428f32515d43c
colibri <adm@colibri.pe>
2021-07-07 22:17:34 -0500

    first commit

--- config/settings.py
+++ config/settings.py
@@ -138,8 +138,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [str(BASE_DIR.joinpath('static'))] # new
 STATIC_ROOT = str(BASE_DIR.joinpath('staticfiles')) # new
-STATICFILES_STORAGE =
-'whitenoise.storage.CompressedManifestStaticFilesStorage' # new
+STATICFILES_STORAGE ='whitenoise.storage.CompressedManifestStaticFilesStorage' # new
 
 # Autenticacion de User
 AUTH_USER_MODEL = 'accounts.CustomUser'
